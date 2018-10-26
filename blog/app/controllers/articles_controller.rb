dca230bbc71e05fea43032b37359050cf76f3722
jellythewobbly <jellythewobbly@hotmail.com>
2018-10-26 18:03:25 +0800

    minor update of articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,12 +3,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new
-    @article = Article.new
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def create
@@ -18,6 +18,20 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
